69583132efc33bbcc42a2e055758e42073631269
Aaron Smith <asmith1810@gmail.com>
2017-04-15 23:37:48 -0400

    Move join and list response logic into their own files

--- registrar/server.py
+++ registrar/server.py
@@ -5,12 +5,13 @@ import zmq
 import registrar.Registrar.Command
 import registrar.Registrar.Create
 import registrar.Registrar.Join
-import registrar.Registrar.List
 import registrar.Registrar.Message
 
 from registrar.models.client import Client
 from registrar.models.room import Room
 import registrar.config as config
+import registrar.api.list as list_api
+import registrar.api.join as join_api
 
 PORT = '5556'
 
@@ -28,39 +29,6 @@ def build_command(builder, message_type, message):
     registrar.Registrar.Command.CommandAddMessage(builder, message)
     return registrar.Registrar.Command.CommandEnd(builder)
 
-def build_list_message(builder, rooms):
-    room_offsets = []
-    for room in rooms:
-        offset = room.serialize(builder)
-        room_offsets.append(offset)
-
-    registrar.Registrar.List.ListStartRoomsVector(builder, len(room_offsets))
-    for offset in room_offsets:
-        builder.PrependUOffsetTRelative(offset)
-    room_vector_offset = builder.EndVector(len(room_offsets))
-
-    registrar.Registrar.List.ListStart(builder)
-    registrar.Registrar.List.ListAddRooms(builder, room_vector_offset)
-    return registrar.Registrar.List.ListEnd(builder)
-
-def build_join_message(builder, room, clients):
-    client_offsets = []
-    for client in clients:
-        offset = client.serialize(builder)
-        client_offsets.append(offset)
-
-    room_offset = room.serialize(builder)
-
-    registrar.Registrar.Join.JoinStartClientsVector(builder, len(client_offsets))
-    for offset in client_offsets:
-        builder.PrependUOffsetTRelative(offset)
-    client_vector_offset = builder.EndVector(len(client_offsets))
-
-    registrar.Registrar.Join.JoinStart(builder)
-    registrar.Registrar.Join.JoinAddRoom(builder, room_offset)
-    registrar.Registrar.Join.JoinAddClients(builder, client_vector_offset)
-    return registrar.Registrar.Join.JoinEnd(builder)
-
 while True:
     #  Wait for next request from client
     request = socket.recv()
@@ -74,7 +42,7 @@ while True:
     if message_type == registrar.Registrar.Message.Message().List:
         print('SERVER: Received a List command')
         rooms = Room.select()
-        message_offset = build_list_message(builder, rooms)
+        message_offset = list_api.response(builder, rooms)
     elif message_type == registrar.Registrar.Message.Message().Create:
         print('SERVER: Received a Create command')
         union_create = registrar.Registrar.Create.Create()
@@ -96,7 +64,7 @@ while True:
         room = Room.get(Room.guid == guid)
 
         # Send room and existing clients to the requester
-        message_offset = build_join_message(builder, room, room.clients)
+        message_offset = join_api.response(builder, room, room.clients)
 
         # Add the requester to the list of clients
         with db.transaction():
