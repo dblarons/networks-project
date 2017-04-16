418c0d006755cee87d5c821e0a8d37bfe669ca46
Aaron Smith <asmith1810@gmail.com>
2017-04-15 21:48:25 -0400

    Clean up server

--- registrar/server.py
+++ registrar/server.py
@@ -25,19 +25,19 @@ def build_command(builder, message_type, message):
     registrar.Registrar.Command.CommandAddMessage(builder, message)
     return registrar.Registrar.Command.CommandEnd(builder)
 
-def build_list_message(builder, room_models):
+def build_list_message(builder, rooms):
     room_offsets = []
-    for room in room_models:
+    for room in rooms:
         offset = room.serialize(builder)
         room_offsets.append(offset)
 
     registrar.Registrar.List.ListStartRoomsVector(builder, len(room_offsets))
     for offset in room_offsets:
         builder.PrependUOffsetTRelative(offset)
-    rooms = builder.EndVector(len(room_offsets))
+    room_vector_offset = builder.EndVector(len(room_offsets))
 
     registrar.Registrar.List.ListStart(builder)
-    registrar.Registrar.List.ListAddRooms(builder, rooms)
+    registrar.Registrar.List.ListAddRooms(builder, room_vector_offset)
     return registrar.Registrar.List.ListEnd(builder)
 
 while True:
@@ -45,19 +45,20 @@ while True:
     request = socket.recv()
 
     Command = registrar.Registrar.Command.Command.GetRootAsCommand(request, 0)
-    message_type = Command.MessageType()
 
     # Switch on the message type to send a response.
     builder = flatbuffers.Builder(1024)
     message_offset = 0
-    if message_type == registrar.Registrar.Message.Message().List:
+    message_type = None
+    if Command.MessageType() == registrar.Registrar.Message.Message().List:
         print('SERVER: Received a List command')
         rooms = Room.select()
+        message_type = registrar.Registrar.Message.Message().List
         message_offset = build_list_message(builder, rooms)
 
     offset = build_command(
         builder,
-        registrar.Registrar.Message.Message().List,
+        message_type,
         message_offset)
 
     builder.Finish(offset)
